100023b9426c9139f1babf7d5c557f29b4a2b13a
bobshadow66 <eebooby@gmail.com>
2024-06-16 14:40:09 -0700

    yes

--- routers/categories.js
+++ routers/categories.js
@@ -1,6 +1,7 @@
 const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
+const bcrypt =require(bycrpt.js)
 
 router.get('/', async (req, res) => {
     try {
--- routers/users.js
+++ routers/users.js
@@ -14,4 +14,30 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        let user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
+            phone: req.body.street,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            city: req.body.city,
+            country: req.body.country
+        });
+        user = await user.save();
+       
+         if (!category) {
+            return res.status(400).send('The category cannot be created!');
+        }
+        res.send(category);
+    } catch (err) {
+        res.status(500).json({ success: false, error: err });
+    }
+});
+
 module.exports = router;
