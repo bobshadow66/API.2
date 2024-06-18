7a632ab070392b17d7d55d05d2af92a505299ec7
bobshadow66 <eebooby@gmail.com>
2024-06-18 13:24:20 -0700

    yes

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,18 @@
-const expressJwt = require('exoress-jwt');
+const expressJwt = require('express-jwt');
 
 function authJwt() {
-    const secret = process.env.secret;
+    const secret = process.env.SECRET;
+    const api = process.env.API_URL;
     return expressJwt({
         secret,
-        algorithm: ['HS256'] 
-    })
+        algorithms: ['HS256']  
+    }).unless({
+        path: [
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            '${api}/users/login',
+            '${api}users/register'
+        ]
+    });
 }
 
-module.exports = authJwt; 
+module.exports = authJwt;
--- routers/app.js
+++ routers/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 const authJwt = require('./helpers/jwt');
-
+const errorHandler = require('./helpers/error-handler')
 app.use(cors());
 app.options('*', cors());
 
@@ -14,19 +14,30 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
-app.use((err, req, res, next) => { 
-    if (err) {
-        res.status(500).json({ message: "error in the server" });
-    } else {
-        next();
-    }
-});
+app.use(errorHandler )
 
 // Routes
 const categoriesRoutes = require('./routes/categories');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
 
 app.use('/categories', categoriesRoutes);
 app.use('/products', productsRoutes);
 app.use('/users', usersRoutes);
+app.use('${api}/orders', ordersRoutes);
+
+//Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(() => {
+    console.log('Database Connection is ready...');
+})
+.catch((err) => {
+    console.log(err);
+});
