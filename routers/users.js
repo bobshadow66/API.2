8c0990de28fb8550e6daa545e3190ddf444d6251
bobshadow66 <eebooby@gmail.com>
2024-06-25 11:50:02 -0700

    no

--- routers/users.js
+++ routers/users.js
@@ -114,25 +114,61 @@ router.post('/login', async (req, res) => {
 });
 
 
-router.post('/register', async (req,res))=>{
-    let user = new User ({
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        street: req.body.street,
-        apartment: req.body.apartment,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country
-    })
-    user = await user.save();
+router.post('/register', async (req, res) => { // Fixed function syntax
+    try {
+        let user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: bcrypt.hashSync(req.body.password, 10),
+            phone: req.body.phone,
+            isAdmin: req.body.isAdmin,
+            street: req.body.street,
+            apartment: req.body.apartment,
+            zip: req.body.zip,
+            city: req.body.city,
+            country: req.body.country
+        });
+        
+        user = await user.save();
 
-    if(!user)
-    return res.status(400).send('the user cannot be created')
-    
-    res.send(user); 
-}
+        if (!user) {
+            return res.status(400).send('The user cannot be created');
+        }
+
+        res.send(user);
+    } catch (err) {
+        res.status(500).json({ success: false, error: err });
+    }
+});
+
+router.get('/get/count', async (req, res) => {
+    try {
+        const userCount = await Product.countDocuments();
+
+        if (userCount === 0) {
+            return res.status(500).json({ success: false });
+        }
+
+        res.send({
+            userCount: userCount
+        });
+    } catch (err) {
+        res.status(500).json({ success: false, error: err });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndRemove(req.params.id);
+        if (user) {
+            return res.status(200).json({ success: true, message: 'The user is deleted' });
+        } else {
+            return res.status(404).json({ success: false, message: 'User not found' });
+        }
+    } catch (err) {
+        return res.status(500).json({ success: false, error: err });
+    }
+});
+
 
 module.exports = router;
