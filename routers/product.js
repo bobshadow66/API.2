6872a21bc3267cce916ef99f4b0981551541bf9f
bobshadow66 <eebooby@gmail.com>
2024-07-01 15:52:24 -0700

    no

--- routers/product.js
+++ routers/product.js
@@ -7,11 +7,11 @@ const multer = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/tmp/my-uploads');
+        cb(null, 'public/uploads');
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix);
+        const fileName = file.originalname.split(' ').json('-');
+        cb(null, fileName + '-' + Date.now());
     }
 });
 
